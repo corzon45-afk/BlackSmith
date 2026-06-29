77ba21566b2ac87589cb87e152e1e94d4e0748d3
corzon45-afk <corzon45@gmail.com>
2026-06-28 23:09:14 -0300

    Update funcion.js

--- funcion.js
+++ funcion.js
@@ -2,7 +2,7 @@
 // CONFIGURACIÓN Y ESTADO GLOBAL
 // ==========================================
 const SHEET_URL = "https://script.google.com/macros/s/AKfycbxWI9_CeLkxqqpRxfvZWkj9jlzkdJQXYuOMJwnAoSzROwKBC1pJ0nSKfWyqRssqR5qEGg/exec";
-const SHEET_ITEMS_URL = "https://script.google.com/macros/s/AKfycbxgzUMaSWOv-mTzb5m0xOp0KnXPCVKtb3UVUoO1Rs5IodZQEDmy-O5nKq9GrGMUT_IJew/exec"; 
+const SHEET_ITEMS_URL = "https://script.google.com/macros/s/AKfycbyaq2P-hPtB5ZalVIzKImJUNJEO6VinVSxX3pCKc-4m0AvMpP33oWJ5dANy-7jJzi2tCw/exec"; 
 
 let data = { pc: [], mob: [], items: [] };
 let isLoading = true;
